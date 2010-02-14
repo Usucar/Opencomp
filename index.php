82fe27f9129ab2ecf67cb382c0c5abf9766cc904
jtraulle <jtraulle@837a5888-1cfb-11df-8821-87deac751e95>
2010-02-14 16:08:14 +0000

    Réécriture du code from scratch, début de la v. -0.3
    git-svn-id: http://opencomp.googlecode.com/svn/trunk@3 837a5888-1cfb-11df-8821-87deac751e95

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+
 /*
  * ========================================================================
- * Copyright (C) 2008 Sadaoui Akim
+ * Copyright (C) 2010 Traullé Jean
  *
- * This program is free software; you can redistribute it and/or modify
+ * This file is part of Gnote.
+ *
+ * Gnote is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
@@ -14,11 +17,10 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * with Gnote. If not, see <http://www.gnu.org/licenses/>
  * ========================================================================
  */
-	include ("commons.php");
-	printHead('Accueil');
-	include ("foot.php");
+
+header('Refresh: 0; url=auth.php');
+
 ?>
