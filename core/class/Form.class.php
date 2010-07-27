98693623f1038c11d9cb3e0708d48c064679ccd1
jtraulle <jtraulle@837a5888-1cfb-11df-8821-87deac751e95>
2010-07-27 22:28:47 +0000

    Amélioration de la class Form
    git-svn-id: http://opencomp.googlecode.com/svn/trunk@38 837a5888-1cfb-11df-8821-87deac751e95

--- core/class/Form.class.php
+++ core/class/Form.class.php
@@ -23,24 +23,28 @@
 
 class Form
 {
-	private $echo;
-	private $errors = 0;
-	private $tabaccessid;
 
-	public function __construct()
-	{
-		echo '<form method="post" action="'.htmlentities($_SERVER['PHP_SELF']).'">';
-	}
+    private $echo;
+    private $errors = 0;
+    private $tabaccessid;
+
+    public function __construct()
+    {
+	echo '<form method="post" action="'.htmlentities($_SERVER['PHP_SELF']).'">';
+    }
 
 
 
-	/*  Cette fonction permet de créer un champ de type input text
-	 *
-	 *  Elle prend pour paramètre $field qui servira pour l'id et le name du champ
-	 *  $lablel permet de définir le label du champ input text
-	 *  $type permet de choisir le type de validation souhaité.
+	/**
+         * Cette méthode permet de créer un champ de type input text et de le vérifier
+         * 
+         * @param string $field Nom du champ
+         * @param string $label Nom du label (optionel)
+         * @param string $type Type du champ à choisir parmi 'alpha','alpha_avec_accent', 'numerique'
+         * @param int $longueur_mini Longueur minimale de la chaîne de caractères
+         * @param int $longueur_maxi Longueur maximale de la chaîne de caractères
 	 */
-	public function input($field,$label,$type)
+	public function input($field,$label,$type,$longueur_mini,$longueur_maxi)
 	{
 		if (isset($_POST['submit']))
 		{
@@ -48,57 +52,80 @@ class Form
 			{
 				case 'alpha':
 
-				if (!preg_match("#^[[:alpha:]]{1,}$#", htmlspecialchars($_POST['data_'.$field])))
+				if (!preg_match('#^[[:alpha:]]{'.$longueur_mini.','.$longueur_maxi.'}$#', htmlspecialchars($_POST['data_'.$field])))
 				{
-					$return = '<label for="form_'.$field.'" >'.$label.'</label><input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" />';
-					$this->echo[] = $return;
-					$this->echo[] = '<span class="tooltip"><img src="../style/img/error.png" /> <em><span></span>Vous devez saisir une chaine alphabétique sans accent !</em></span><br />';
+					if(!empty ($label))
+                                        {
+                                            $this->echo[] = '<label for="form_'.$field.'" >'.$label.'</label>';
+                                        }
+                                        $this->echo[] = '<input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" />';
+					$this->echo[] = '<span class="tooltip"><img src="../style/img/error.png" /> <em><span></span>Vous devez saisir une chaine alphabétique sans accent d\'une longueur minimale de '.$longueur_mini.' caractère(s) !</em></span><br />';
 
 					$this->errors++;
 				}
 
 				else
 				{
-					$return = '<label for="form_'.$field.'" >'.$label.'</label><input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" /><br />';
-					$this->echo[] = $return;
+                                    if(!empty ($label))
+                                    {
+                                        $this->echo[] = '<label for="form_'.$field.'" >'.$label.'</label>';
+                                    }
+
+                                    $this->echo[] = '<input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" /><br />';
 				}
 
 				break;
 
                                 case 'alpha_avec_accent':
 
-				if (!preg_match("#^[a-zA-ZâêôûÄéÇàèÊùÌÍÎÏîÒÓÔÕÖÙÚÛÜàáâãäçèéêëìíîïñòóôõöùúûü]{1,}$#", htmlspecialchars($_POST['data_'.$field])))
+				if (!preg_match('#^[a-zA-ZâêôûÄéÇàèÊùÌÍÎÏîÒÓÔÕÖÙÚÛÜàáâãäçèéêëìíîïñòóôõöùúûü]{'.$longueur_mini.','.$longueur_maxi.'}$#', htmlspecialchars($_POST['data_'.$field])))
 				{
-					$return = '<label for="form_'.$field.'" >'.$label.'</label><input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" />';
-					$this->echo[] = $return;
-					$this->echo[] = '<span class="tooltip"><img src="../style/img/error.png" /> <em><span></span>Vous devez saisir une chaine alphabétique !</em></span><br />';
+                                        if(!empty ($label))
+                                        {
+                                            $this->echo[] = '<label for="form_'.$field.'" >'.$label.'</label>';
+                                        }
+                                    
+                                        $this->echo[] = '<input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" />';
+					$this->echo[] = '<span class="tooltip"><img src="../style/img/error.png" /> <em><span></span>Vous devez saisir une chaine alphabétique d\'une longueur minimale de '.$longueur_mini.' caractère(s) !</em></span><br />';
 
 					$this->errors++;
 				}
 
 				else
 				{
-					$return = '<label for="form_'.$field.'" >'.$label.'</label><input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" /><br />';
-					$this->echo[] = $return;
+                                    if(!empty ($label))
+                                    {
+                                        $this->echo[] = '<label for="form_'.$field.'" >'.$label.'</label>';
+                                    }
+
+                                    $this->echo[] = '<input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" /><br />';
 				}
 
 				break;
 
                                 case 'numerique':
 
-				if (!preg_match("#^[[:digit:]]{1,}$#", htmlspecialchars($_POST['data_'.$field])))
+				if (!preg_match('#^[[:digit:]]{'.$longueur_mini.','.$longueur_maxi.'}$#', htmlspecialchars($_POST['data_'.$field])))
 				{
-					$return = '<label for="form_'.$field.'" >'.$label.'</label><input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" />';
-					$this->echo[] = $return;
-					$this->echo[] = '<span class="tooltip"><img src="../style/img/error.png" /> <em><span></span>Vous devez saisir une chaine numérique !</em></span><br />';
+					if(!empty ($label))
+                                        {
+                                            $this->echo[] = '<label for="form_'.$field.'" >'.$label.'</label>';
+                                        }
+                                    
+                                        $this->echo[] = '<input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" />';
+					$this->echo[] = '<span class="tooltip"><img src="../style/img/error.png" /> <em><span></span>Vous devez saisir une chaine numérique d\'une longueur minimale de '.$longueur_mini.' caractère(s) !</em></span><br />';
 
 					$this->errors++;
 				}
 
 				else
 				{
-					$return = '<label for="form_'.$field.'" >'.$label.'</label><input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" /><br />';
-					$this->echo[] = $return;
+                                    if(!empty ($label))
+                                    {
+                                        $this->echo[] = '<label for="form_'.$field.'" >'.$label.'</label>';
+                                    }
+                                        
+                                    $this->echo[] = '<input type="text" name="data_'.$field.'" id="form_'.$field.'" value="'.$_POST['data_'.$field].'" /><br />';
 				}
 
 				break;
@@ -108,11 +135,20 @@ class Form
 		}
 		else
 		{
-			$return = '<label for="form_'.$field.'" >'.$label.'</label><input type="text" name="data_'.$field.'" id="form_'.$field.'" /><br />';
-			$this->echo[] = $return;
+                    if(!empty ($label))
+                    {
+                        $this->echo[] = '<label for="form_'.$field.'" >'.$label.'</label>';
+                    }
+
+                    $this->echo[] = '<input type="text" name="data_'.$field.'" id="form_'.$field.'" /><br />';
 		}
 	}
 
+
+
+        /**
+         * Cette méthode permet de savoir si des erreurs ont été rencontrés lors de la validation du formulaire. Elle retourne donc true si c'est le cas.
+	 */
 	public function iserrors()
 	{
 		if ($this->errors != 0)
@@ -121,38 +157,53 @@ class Form
 		}
 	}
 
-	public function submit($value)
+	/**
+         * Cette méthode permet d'afficher le bouton submit et de clore le formulaire en fermant la balise <form>
+         *
+         * @param string $value Valeur du bouton submit
+	 */
+        public function submit($value)
 	{
 		$this->echo[] = '<input type="submit" name="submit" id="submit" value="'.$value.'" /></form>';
 	}
 
+
+	/**
+         * Cette méthode permet d'afficher le formulaire après avoir vérifié si celui ci comportait des erreurs dans le cas ou le formulaire aurait déjà été envoyé.
+         * Si c'est le cas, on affiche une alerte avant d'afficher le formulaire !
+	 */
 	public function afficher_formulaire()
 	{
-		if ($this->iserrors())
-		{
-			echo '<ul style="margin-bottom:10px; padding:0px;"><li class="error" id="error">Des erreurs ont été détectées lors de la validation des données saisies dans le formulaire !</li></ul>';
+            //On vérifie d'abord si il y a eu des erreurs dans le cas ou le formulaire aurait déjà été envoyé.
+            //Si c'est le cas, on affiche une alerte
+            if ($this->iserrors())
+            {
+		echo '<ul style="margin-bottom:10px; padding:0px;"><li class="error" id="error">Des erreurs ont été détectées lors de la validation des données saisies dans le formulaire !</li></ul>';
 
-			?>
+                ?>
 
-			<script type='text/javascript'>
+                <script type='text/javascript'>
 
-				window.setTimeout(function() {
-					new Effect.Highlight('error', { startcolor: '#FFFFFF', endcolor: '#FFBDBD', restorecolor: '#FFE7E7',keepBackgroundImage: 'true' });
-				}, 200);
+		window.setTimeout(function()
+                {
+                    new Effect.Highlight('error', { startcolor: '#FFFFFF', endcolor: '#FFBDBD', restorecolor: '#FFE7E7',keepBackgroundImage: 'true' });
+		}, 200);
 
-				window.setTimeout(function() {
-					new Effect.Fade('error');
-				}, 10000);
+		window.setTimeout(function()
+                {
+                    new Effect.Fade('error');
+		}, 10000);
 
-			</script>
+		</script>
 
-			<?php
-		}
+		<?php
+            }
 
-		foreach ($this->echo as $retour)
-		{
-			echo $retour;
-		}
+            //Puis on affiche la vue stockée dans la variable echo à l'utilisateur
+            foreach ($this->echo as $retour)
+            {
+		echo $retour;
+            }
 	}
 
 }
